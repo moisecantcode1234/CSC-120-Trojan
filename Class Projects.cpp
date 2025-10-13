8852b8c9c7148b3cacfa2df4be6a4a92a5acc68c
Moïse Anglade <104998624+moisecantcode1234@users.noreply.github.com>
2025-10-12 22:19:01 -0400

    Hmm *_*
    More stuff...

--- Class Projects.cpp
+++ Class Projects.cpp
@@ -10,12 +10,14 @@ int main()
 {
     const float maTax = .0675;
     float meal, total, tip, radius;
+    char kchoice;
     string fName, lName, rFeedback;
+    string full, msg;
 
     //printf("Hello WOrld");s
     cout << "Welcome to the Guilded Hare!\n";
     cout << "============================\n";
-
+/*
     cout << "Enter your first name: ";
     getline(cin, fName);
     cout << "Enter your last name: ";
@@ -62,6 +64,112 @@ int main()
         cout << "There are no spaces in your feedback. Idk why tho -_-\n";
     else
     cout << "The first space in your feedback is at position: " << rFeedback.find(" ") << "\n";
+
+    full = fName + " " + lName;
+    cout << "\033[31m"; // clear screen
+    msg = ("Thank you " + full + " for dining with us!");
+    cout << msg.length() << endl;
+    cout << msg << "\n";
+
+    cout << "Press any key to continue...\n";
+    cin >> kchoice;
+    for (int i = 0; i <= 4; i++)
+    {
+        for (int j = 0; j < 6 - i; j++)
+        {
+            cout << " ";
+        }
+        string choice (1 + (2 * i), kchoice);
+        cout << choice << endl;
+    }
+    cout << "\033[0m"; // reset color
+    */
+    int score;
+    cout << "Enter your score (0-100): ";
+    cin >> score;
+    if (score >= 90)
+        cout << "You got an A!\n";
+    else if (score >= 80)
+        cout << "You got a B!\n";
+    else if (score >= 70)
+        cout << "You got a C!\n";
+    else if (score >= 60)
+        cout << "You got a D!\n";
+    else {
+        cout << "You got an F!\n";
+    }
+
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
+    if (age < 0 )
+    {
+        cout << "You are not born yet";
+    }
+
+    
+
+
+
+
+    cout << "Enter three numbers:";
+    float a,b,c, grateater,mid, lesser;
+    cin >> a >> b >> c;
+
+    if (a > b && a > c)
+        grateater = a;
+    else if (b > a && b > c ) 
+        grateater = b;
+    else if (c > a && c > b)
+        grateater = c;
+    else if (a < b && a < c)
+        lesser = a;
+    else if (b < a && b < c)
+        lesser = b;
+    else if (c < a && c < b)
+        lesser = c;
+    cout << "The greatest number is: " << grateater << "\n";
+    cout << "The lesser number is: " << lesser << "\n";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     return 0;
 }
---
+++ Froth.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Just curious, what is your GPA? (0.0 - 4.0) : ";
+    float gpa;
+    cin >> gpa;
+    if (gpa < 0.0 || gpa > 4.0) {
+        cout << "Invalid GPA input. Please enter a value between 0.0 and 4.0.\n";
+        cin >> gpa;
+        if (gpa < 0.0 || gpa > 4.0) {
+            cout << "Try again. Exiting program.\n";
+            return 1;
+        }
+    }  
+    cout << "Did you perhaps participate in any extracurricular activities? (1 for yes, 0 for no): ";
+    bool exChoice;
+    cin >> exChoice;
+    cout << "How many hours of credits do you have? : ";
+    float credits;
+    cin >> credits;
+    
+    if (gpa >= 3.5 && credits >= 30 )
+        cout << "Congratulations, you qualify for our scholarship!\n";
+    else if (gpa > 2.5 && exChoice)
+        cout << "You are only Conditionally eligible for our scholarship.\n";
+    else{
+        cout << "You're going to need to work harder in life.\n";
+    }
+
+    return 0;
+}
